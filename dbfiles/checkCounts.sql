ece7f1d19401b2e5cf038235950921d0a5a94415
chagit <cassulin@g.jct.ac.il>
2026-04-28 14:28:31 +0300

    feat: add database schema, data seeding (SQL/CSV/Python), queries and backup scripts

---
+++ dbfiles/checkCounts.sql
@@ -0,0 +1,14 @@
+-- checkCounts.sql
+-- Check number of records in each table
+
+SELECT 'roles' AS table_name, COUNT(*) AS total_rows FROM roles
+UNION ALL SELECT 'departments', COUNT(*) FROM departments
+UNION ALL SELECT 'users', COUNT(*) FROM users
+UNION ALL SELECT 'access_logs', COUNT(*) FROM access_logs
+UNION ALL SELECT 'cctv_cameras', COUNT(*) FROM cctv_cameras
+UNION ALL SELECT 'incidents', COUNT(*) FROM incidents
+UNION ALL SELECT 'it_assets', COUNT(*) FROM it_assets
+UNION ALL SELECT 'system_backups', COUNT(*) FROM system_backups
+UNION ALL SELECT 'incident_assignments', COUNT(*) FROM incident_assignments
+UNION ALL SELECT 'asset_incident', COUNT(*) FROM asset_incident
+ORDER BY table_name;
---
+++ dbfiles/selectAll.sql
@@ -0,0 +1,13 @@
+-- selectAll.sql
+-- Select all records from all tables
+
+SELECT * FROM roles;
+SELECT * FROM departments;
+SELECT * FROM users;
+SELECT * FROM access_logs;
+SELECT * FROM cctv_cameras;
+SELECT * FROM incidents;
+SELECT * FROM it_assets;
+SELECT * FROM system_backups;
+SELECT * FROM incident_assignments;
+SELECT * FROM asset_incident;
